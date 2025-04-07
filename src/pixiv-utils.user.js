14c92dc772c938d4baf0851302e0dcc3bc13b961
Bobby Wibowo <bobbywibowo@live.com>
2025-04-07 15:26:24 +0700

    feat(pixiv-utils): 1.5.2

--- src/pixiv-utils.user.js
+++ src/pixiv-utils.user.js
@@ -10,7 +10,7 @@
 // @grant        GM_setValue
 // @grant        window.onurlchange
 // @run-at       document-start
-// @version      1.5.1
+// @version      1.5.2
 // @author       Bobby Wibowo
 // @license      MIT
 // @description  7/2/2024, 8:37:14 PM
