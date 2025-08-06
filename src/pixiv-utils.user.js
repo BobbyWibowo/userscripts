ba16b2fbdb091d580fd4a85943b7be92f64a784a
Bobby Wibowo <bobbywibowo@live.com>
2025-08-06 17:00:37 +0700

    feat(pixiv-utils): 1.6.28, update some selectors

--- src/pixiv-utils.user.js
+++ src/pixiv-utils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bobby's Pixiv Utils
 // @namespace    https://github.com/BobbyWibowo
-// @version      1.6.27
+// @version      1.6.28
 // @description  Compatible with mobile. "Edit bookmark" and "Toggle bookmarked" buttons, publish dates conversion, block AI-generated works, block by Pixiv tags, UTags integration, and more!
 // @author       Bobby Wibowo
 // @license      MIT
@@ -259,9 +259,9 @@
       },
       // Artist page
       {
-        selectorParent: '.ftHDDa',
-        selectorHeader: '.cyKDJj',
-        selectorImagesContainer: '.cyKDJj ~ div:not([class])'
+        selectorParent: '.cYFOlQ',
+        selectorHeader: '.aCLRB',
+        selectorImagesContainer: '.aCLRB ~ div:not([class])'
       },
       // Artist page's requests tab
       {
