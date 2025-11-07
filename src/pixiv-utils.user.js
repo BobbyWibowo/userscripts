c8d469c12a889c525d9312b121b59551ecd772de
Bobby Wibowo <bobbywibowo@live.com>
2025-11-07 22:20:36 +0700

    feat(pixiv-utils): 1.6.37, version bump
    have not been 100% updated or future-proofed, but most things are working fine now
    
    i'm too busy to update this more for now, so we'll make do with this as a release
    
    it's at least better than the previous version where nearly everything was broken

--- src/pixiv-utils.user.js
+++ src/pixiv-utils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bobby's Pixiv Utils
 // @namespace    https://github.com/BobbyWibowo
-// @version      1.6.36
+// @version      1.6.37
 // @description  Compatible with mobile. "Edit bookmark" and "Toggle bookmarked" buttons, publish dates conversion, block AI-generated works, block by Pixiv tags, UTags integration, and more!
 // @author       Bobby Wibowo
 // @license      MIT
