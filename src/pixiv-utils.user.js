5ee2d9455d1cc58a2a5681e6687fca475a954b88
Bobby Wibowo <bobbywibowo@live.com>
2025-11-29 19:43:43 +0700

    feat(pixiv-utils): 1.6.50, fix toggle bookmarked styling

--- src/pixiv-utils.user.js
+++ src/pixiv-utils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bobby's Pixiv Utils
 // @namespace    https://github.com/BobbyWibowo
-// @version      1.6.49
+// @version      1.6.50
 // @description  Compatible with mobile. "Edit bookmark" and "Toggle bookmarked" buttons, publish dates conversion, block AI-generated works, block by Pixiv tags, UTags integration, and more!
 // @author       Bobby Wibowo
 // @license      MIT
@@ -899,6 +899,9 @@
 
   div:has(+ .pixiv_utils_toggle_bookmarked_container) {
     flex-grow: 1;
+  }
+
+  div:has(+ .pixiv_utils_toggle_bookmarked_container):not(:first-child) {
     justify-content: flex-end;
   }
 
